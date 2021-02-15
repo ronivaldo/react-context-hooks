9cc682270dabb053400d3dbeef86db3a62a1e0bd
Ronivaldo <ronivaldo@gmail.com>
2021-02-14 23:30:33 -0300

    10

---
+++ hooksapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SongList from './components/SongLIst';
+
+function App() {
+  return (
+    <div className="App">
+      <SongList />
+    </div>
+  );
+}
+
+export default App;
